ca746fa904232b806f58df4764680629306e12f7
Rafael <rafaeljarczewski@hotmail.com>
2022-06-19 13:19:10 -0300

    refactor: remove whitespaces

--- bubble-sort.js
+++ bubble-sort.js
@@ -4,7 +4,7 @@
  * O(n)   best case  
  */
 
-export      const bubbleSort = function(arr){
+export const bubbleSort = function(arr){
     for(let i=0; i < arr.length; i++){
         for(let j=0; j < arr.length-i-1; j++){
             if(arr[j] > arr[j+1]){
